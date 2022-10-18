ae653a9bbd0dd480cf8c36e119003fe067a313cf
Sahib3 <sahib.bhinder01@gmail.com>
2022-10-18 11:41:55 -0700

    edited the try/except to work on both games on sale and not on sale

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 #url = 'https://store.steampowered.com/app/1490890/Demon_Slayer_Kimetsu_no_Yaiba_The_Hinokami_Chronicles/'
 #url =  'https://store.steampowered.com/app/1332010/Stray/'
 
-urls = ['https://store.steampowered.com/app/1332010/Stray/','https://store.steampowered.com/app/1490890/Demon_Slayer_Kimetsu_no_Yaiba_The_Hinokami_Chronicles/']
+urls = ['https://store.steampowered.com/app/1490890/Demon_Slayer_Kimetsu_no_Yaiba_The_Hinokami_Chronicles/','https://store.steampowered.com/app/1332010/Stray/',
+     ]
 
 
 
@@ -15,25 +16,26 @@ headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
     "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en"}
 
 
-# scrape data from steam, splice price strings and then type cast to float
+# scrape data from steam
 def scrape():
     for url in urls:
         r = requests.get(url,headers=headers)
         soup = BeautifulSoup(r.content,"html.parser")
         
         title = soup.find(id="appHubAppName").get_text()
-        org_priceText = soup.find(class_="game_purchase_price price").get_text().replace(' ','')
-        org_priceText = ''.join(org_priceText.split())
-        org_price = float(org_priceText[4:])
-
         print(title)
-        
+        #return price from string, get rid of all whitespaces, splice string to inculde only the dollar value
+        #use try/except to differnate titles that are on sale and not on sale, as steam changes the html for the price based on the fact that they're is a sale
         try:
+            org_priceText = soup.find(class_="game_purchase_price price").get_text().replace(' ','')
+            org_priceText = ''.join(org_priceText.split())
+            org_price = float(org_priceText[4:])
+            print(org_price) 
+        except:    
             sale_priceText = soup.find(class_="discount_final_price").get_text()
             sale_price = float(sale_priceText[5:])
             print(sale_price)
-        except:
-            print(org_price)
+     
     
     
 
